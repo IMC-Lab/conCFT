23b045d99585a946b1567bc2baeb925a9f3f92a9
Maria Khoudary <makhouda@uci.edu>
2022-04-15 17:37:04 -0700

    delete duplicate plotting code, update condition labels

--- behavioral_analysis.R
+++ behavioral_analysis.R
@@ -1,12 +1,12 @@
 # load packages -----------------------------------------------------------
-library(lme4) #for fitting linear mixed effects models
-library(lmerTest) #for viewing lme4 results
-library(emmeans) #to extract model estimates for plotting
+library(lme4)
+library(lmerTest)
+library(emmeans)
 library(ltm) #to compute cronbachs alpha
-library(sjPlot) #to make pretty lmer summary tables
-library(patchwork) #for combining multiple plots into one figure
-library(paletti) #for making a custom color palette
-library(tidyverse) #for easy data wrangling & beautiful plot-making 
+library(sjPlot)
+library(patchwork)
+library(paletti)
+library(tidyverse)
 
 # load data ---------------------------------------------------------------
 ## session 1 data
@@ -14,7 +14,7 @@ session1_data = list.files('behavData/session1', full.names = TRUE, pattern='mem
 session1_data = do.call(rbind, lapply(session1_data, function(x) { read.csv(x, header = TRUE)} ))
 session1_data <- session1_data %>%
   rename(subID = sub, eventNum = EventNumber, condition=Condition, eventTitle=Title) %>%
-  mutate(condition=factor(condition, levels=c(0,1,2), labels=c('Practice', 'Dispositional', 'Situational'))) %>%
+  mutate(condition=factor(condition, levels=c(0,1,2), labels=c('Practice', 'Internal', 'External'))) %>%
   filter(condition != 'Practice')
 
 ## load in session 2 data
@@ -22,7 +22,7 @@ session2_data = list.files('behavData/session2', full.names = TRUE, pattern='con
 session2_data = do.call(rbind, lapply(session2_data, function(x) { read.csv(x, header = TRUE)[1:33]} ))
 session2_data <- session2_data %>%
   rename(subID = sub) %>%
-  mutate(condition = factor(condition, levels=c(1,2), labels=c('Dispositional', 'Situational')))
+  mutate(condition = factor(condition, levels=c(1,2), labels=c('Internal', 'External')))
 
 ## load in post-study questionnaire (psq)
 psq_data = list.files('behavData/session1', full.names = TRUE, pattern='psq_s.*.csv', recursive = TRUE)
@@ -31,17 +31,17 @@ psq_data <- psq_data %>%
   rename(subID = sub,
          condition = Condition,
          eventTitle = Title) %>%
-  mutate(condition=factor(condition, levels=c('Self','Context'), labels=c('Dispositional', 'Situational')))
+  mutate(condition=factor(condition, levels=c('Self','Context'), labels=c('Internal', 'External')))
 
 
 # tidy data ---------------------------------------------------------------
 ## first create subset of psq_data so that we can add frequency to the main df
 psq <- psq_data %>%
-  filter(subID %in% session2_data$subID) %>%
+  filter(subID %in% session2_data$subID) %>% 
   dplyr::select(eventTitle, condition, subID, Frequency)
 
 ## then get rid of subjects who didn't get scanned, drop memories used for practice, and fix behavioral responses >7
-all_data <- session1_data %>%
+all_data <- session1_data %>% 
   filter(subID %in% session2_data$subID) %>%
   right_join(session2_data, by=c('subID', 'eventNum', 'condition', 'eventTitle'))  %>%
   left_join(psq, by=c('eventTitle', 'condition', 'subID')) %>%
@@ -56,10 +56,18 @@ all_data <- session1_data %>%
 # write.csv(all_data, 'behavData/all_data.csv')
 
 
-# make custom color palette  -----------------------------------------------
-custom_colors <- c('#00a6ff', '#ffa200')
+# make custom color palette & summary stat plotting function -----------------------------------------------
+custom_colors <- c('#00a6ff', '#ffa200')   
 custom_color <- get_scale_color(get_pal(custom_colors))
 custom_fill <- get_scale_fill(get_pal(custom_colors))
+
+mean_sd <- function(x) {
+  m <- mean(x)
+  ymin <- m-sd(x)
+  ymax <- m+sd(x)
+  return(c(y=m,ymin=ymin,ymax=ymax))
+}
+
 
 
 # summary statistics for session 1 ----------------------------------------
@@ -85,16 +93,16 @@ session1_long <- all_data %>%
   pivot_longer(c(Detail, Vividness, Valence, Regret, Control), names_to = 'memory_characteristic', values_to = 'rating') %>%
   mutate(rating=ifelse(rating==8, 7, rating))
 
-## plot
+## plot 
 session1_plot <- function(aes_DV, DV, title) {
   ggplot(all_data, aes_(aes_DV)) +
-    geom_bar(color='black', fill='#57C09B', width=1, alpha=1) +
+    geom_bar(color='black', fill='#57C09B', width=1, alpha=1) + 
     geom_vline(aes(xintercept=mean({{DV}},na.rm=T)), size=0.75, linetype='dashed', color='gray30') +
-    geom_errorbarh(aes(xmin=mean({{DV}},na.rm=T)-sd({{DV}},na.rm=T), xmax=mean({{DV}}, na.rm=T)+sd({{DV}}, na.rm=T), y=0, height=50), size=1) +
-    geom_point(aes(x=mean({{DV}}, na.rm=T), y=0), size=3) +
-    scale_x_continuous(breaks=1:7) +
-    labs(y='Count', x='Rating', title=title) +
-    theme_light() +
+    geom_errorbarh(aes(xmin=mean({{DV}},na.rm=T)-sd({{DV}},na.rm=T), xmax=mean({{DV}}, na.rm=T)+sd({{DV}}, na.rm=T), y=0, height=50), size=1) + 
+    geom_point(aes(x=mean({{DV}}, na.rm=T), y=0), size=3) + 
+    scale_x_continuous(breaks=1:7) + 
+    labs(y='Count', x='Rating', title=title) + 
+    theme_light() + 
     theme(legend.position = 'none',
           axis.text.x=element_text(size=10, color='black'),
           axis.text.y=element_text(size=10, color='black'),
@@ -105,7 +113,7 @@ session1_plot <- function(aes_DV, DV, title) {
 controlHist <- session1_plot(~Control, Control, 'Control')
 detailHist <- session1_plot(~Detail, Detail, 'Detail')
 regretHist <- session1_plot(~Regret, Regret, 'Regret')
-valenceHist <- session1_plot(~Valence, Valence, 'Valence')
+valenceHist <- session1_plot(~Valence, Valence, 'Valence') 
 vividnessHist <- session1_plot(~Vividness, Vividness, 'Vividness')
 controlHist + detailHist + regretHist + valenceHist + vividnessHist
 ggsave('suppFigures/memoryCharacteristics.jpeg', width=10, dpi='retina')
@@ -156,18 +164,18 @@ summary_stat <- session2_summary %>%
 ## plot
 session2_plot <- function(aes_DV, DV, title, n) {
   ggplot(all_data, aes_(x=aes_DV, fill=~condition)) +
-    facet_wrap(vars(condition)) +
-    geom_bar(color='black', width=1, alpha=1) +
-    geom_errorbarh(data=filter(all_data, condition=='Dispositional'), aes(xmin=mean({{DV}},na.rm=T)-sd({{DV}},na.rm=T), xmax=mean({{DV}}, na.rm=T)+sd({{DV}}, na.rm=T), y=0, height=50), size=1) +
-    geom_errorbarh(data=filter(all_data, condition=='Situational'),  aes(xmin=mean({{DV}},na.rm=T)-sd({{DV}},na.rm=T), xmax=mean({{DV}}, na.rm=T)+sd({{DV}}, na.rm=T), y=0, height=50), size=1) +
-    geom_point(data=filter(all_data, condition=='Dispositional'), aes(x=mean({{DV}},na.rm=T), y=0), size=3) +
-    geom_point(data=filter(all_data, condition=='Situational'), aes(x=mean({{DV}},na.rm=T), y=0), size=3) +
-    geom_vline(data=filter(all_data, condition=='Dispositional'), aes(xintercept=mean({{DV}},na.rm=T)), size=0.75, linetype='dashed', color='gray30') +
-    geom_vline(data=filter(all_data, condition=='Situational'), aes(xintercept=mean({{DV}},na.rm=T)), size=0.75, linetype='dashed', color='gray30') +
-    scale_x_continuous(breaks=1:n) +
+    facet_wrap(vars(condition)) + 
+    geom_bar(color='black', width=1, alpha=1) + 
+    geom_errorbarh(data=filter(all_data, condition=='Internal'), aes(xmin=mean({{DV}},na.rm=T)-sd({{DV}},na.rm=T), xmax=mean({{DV}}, na.rm=T)+sd({{DV}}, na.rm=T), y=0, height=50), size=1) +
+    geom_errorbarh(data=filter(all_data, condition=='External'),  aes(xmin=mean({{DV}},na.rm=T)-sd({{DV}},na.rm=T), xmax=mean({{DV}}, na.rm=T)+sd({{DV}}, na.rm=T), y=0, height=50), size=1) +
+    geom_point(data=filter(all_data, condition=='Internal'), aes(x=mean({{DV}},na.rm=T), y=0), size=3) +
+    geom_point(data=filter(all_data, condition=='External'), aes(x=mean({{DV}},na.rm=T), y=0), size=3) +
+    geom_vline(data=filter(all_data, condition=='Internal'), aes(xintercept=mean({{DV}},na.rm=T)), size=0.75, linetype='dashed', color='gray30') +
+    geom_vline(data=filter(all_data, condition=='External'), aes(xintercept=mean({{DV}},na.rm=T)), size=0.75, linetype='dashed', color='gray30') +
+    scale_x_continuous(breaks=1:n) + 
     custom_fill() +
-    labs(y='Count', x='Rating', title=title) +
-    theme_light() +
+    labs(y='Count', x='Rating', title=title) + 
+    theme_light() + 
     theme(legend.position = 'none',
           strip.background = element_rect(fill='gray90'),
           strip.text = element_text(color='black', size=14),
@@ -184,29 +192,6 @@ frequencyHist <- session2_plot(~Frequency, Frequency, 'Frequency of eCFT generat
 controlHist2 + difficultyHist + plausibilityHist + frequencyHist + plot_layout(nrow=4)
 ggsave('suppFigures/eCFTCharacteristics.jpeg', width=10, height=10, dpi='retina')
 
-
-ggplot(all_data, aes(x=controlResponse, fill=condition)) +
-    facet_wrap(vars(condition)) +
-    geom_bar(color='black', width=1, alpha=0.7) +
-    geom_errorbarh(data=filter(all_data, condition=='Dispositional'), aes(xmin=mean(controlResponse, na.rm=T)-sd(controlResponse, na.rm=T), xmax=mean(controlResponse,na.rm=T)+sd(controlResponse, na.rm=T), y=0, height=50), size=1) +
-    geom_errorbarh(data=filter(all_data, condition=='Situational'), aes(xmin=mean(controlResponse, na.rm=T)-sd(controlResponse, na.rm=T), xmax=mean(controlResponse,na.rm=T)+sd(controlResponse, na.rm=T), y=0, height=50), size=1)
-    geom_point(data=filter(summary_stat, condition=='Dispositional'), aes(x=controlMean, y=0), size=3) +
-    geom_point(data=filter(summary_stat, condition=='Situational'), aes(x=controlMean, y=0), size=3) +
-    geom_vline(data=filter(summary_stat, condition=='Dispositional'), aes(xintercept=controlMean), size=0.75, linetype='dashed', color='gray30') +
-    geom_vline(data=filter(summary_stat, condition=='Situational'), aes(xintercept=controlMean), size=0.75, linetype='dashed', color='gray30') +
-    scale_x_continuous(breaks=1:n) +
-    custom_fill() +
-    labs(y='Count', x='Rating', title=title) +
-    theme_light() +
-    theme(legend.position = 'none',
-          strip.background = element_rect(fill='gray90'),
-          strip.text = element_text(color='black', size=14),
-          axis.text.x=element_text(size=10, color='black'),
-          axis.text.y=element_text(size=10, color='black'),
-          axis.title = element_text(size=12),
-          plot.title=element_text(size=16, hjust=0.5, face='bold'))
-
-
 # fit linear mixed effects models ---------------------------------------------
 
 ## perceived control
@@ -219,7 +204,7 @@ tab_model(controlModel,
           strings=c(stat='Beta estimate', ci='CI (95%)', p='p value'),
           dv.labels = 'Perceived control over eCFT',
           col.order = c('est','ci','df.error','stat','p'),
-          pred.labels=c('Intercept', 'Condition (situational)'))
+          pred.labels=c('Intercept', 'Condition (External)'))
 
 ## difficulty
 difficultyModel <- lmer(difficultyResponse ~ condition + (1+condition|subID), all_data)
@@ -231,7 +216,7 @@ tab_model(difficultyModel,
           strings=c(stat='Beta estimate', ci='CI (95%)', p='p value'),
           dv.labels = 'Difficulty of eCFT generation',
           col.order = c('est','ci','df.error','stat','p'),
-          pred.labels=c('Intercept', 'Condition (situational)'))
+          pred.labels=c('Intercept', 'Condition (External)'))
 
 ## plausibility
 plausibilityModel <- lmer(plausibilityResponse ~ condition + (1+condition|subID), all_data)
@@ -243,7 +228,7 @@ tab_model(plausibilityModel,
           strings=c(stat='Beta estimate', ci='CI (95%)', p='p value'),
           dv.labels = 'Perceived plausibility of eCFT',
           col.order = c('est','ci','df.error','stat','p'),
-          pred.labels=c('Intercept', 'Condition (situational)'))
+          pred.labels=c('Intercept', 'Condition (External)'))
 
 ## frequency
 frequencyModel <- lmer(Frequency ~ condition + (1+condition|subID), all_data)
@@ -254,7 +239,7 @@ tab_model(frequencyModel,
           strings=c(stat='Beta estimate', ci='CI (95%)', p='p value'),
           dv.labels = 'Rumination on eCFT (measured as frequency of imagination',
           col.order = c('est','ci','df.error','stat','p'),
-          pred.labels=c('Intercept', 'Condition (situational)'))
+          pred.labels=c('Intercept', 'Condition (External)'))
 
 # plot model results ------------------------------------------------------
 modelPlot <- function(model, raw_y, ylab, title) {
@@ -263,8 +248,8 @@ modelPlot <- function(model, raw_y, ylab, title) {
     ggplot(aes(x=condition, y=emmean, fill=condition)) +
     theme_light() +
     geom_violin(aes_(y=raw_y), data=all_data, bw=0.4) +
-    geom_pointrange(aes(ymin=lower.CL, ymax=upper.CL), size=0.8) +
-    custom_fill() +
+    geom_pointrange(aes(ymin=lower.CL, ymax=upper.CL), size=0.8) + 
+    custom_fill() + 
     labs(y=ylab, x=NULL, title=title) +
     theme(text = element_text(size=14, color='black'),
           plot.title = element_text(hjust=0.5, face='bold'),
@@ -276,14 +261,14 @@ controlPlot <- modelPlot(controlModel, ~controlResponse, 'Perceived control', 'C
 ggsave('suppFigures/controlPlot.jpeg', width=8, dpi='retina')
 
 difficultyPlot <- modelPlot(difficultyModel, ~difficultyResponse, 'Difficulty of generation', 'Difficulty model')
-ggsave('suppFigures/difficultyPlot.jpeg', width=8, dpi='retina')
+ggsave('suppFigures/difficultyPlot.jpeg', width=8, dpi='retina')  
 
 plausibilityPlot <- modelPlot(plausibilityModel, ~plausibilityResponse, 'Perceived plausibility', 'Plausibility model')
-ggsave('suppFigures/plausibilityPlot.jpeg', width=8, dpi='retina')
+ggsave('suppFigures/plausibilityPlot.jpeg', width=8, dpi='retina') 
 
-frequencyPlot <- modelPlot(frequencyModel, ~Frequency, 'Frequency of imagination', 'Frequency model') +
+frequencyPlot <- modelPlot(frequencyModel, ~Frequency, 'Frequency of imagination', 'Frequency model') + 
   scale_y_continuous(breaks=c(1:7))
-ggsave('suppFigures/frequencyPlot.jpeg', width=8, dpi='retina')
+ggsave('suppFigures/frequencyPlot.jpeg', width=8, dpi='retina') 
 
 difficultyPlot + controlPlot + plausibilityPlot + frequencyPlot + plot_layout(ncol=4)
 ggsave('suppFigures/modelsPlot.jpeg', width=14, dpi='retina')
